d567f995b500c1617efd7f5450780aec2ba97e39
Li-hype <livia.m.rodrigues@aluno.senai.br>
2024-10-16 10:46:14 -0300

    Exercicios back end

---
+++ Atividade05/Atividade05/Program.cs
@@ -0,0 +1,54 @@
+﻿using Atividade05;
+
+//Instanciando aclasse Funcionário
+Funcionario func1 = new Funcionario { Nome = "Lívia", Idade = 17 };
+Funcionario func2 = new Funcionario { Nome = "Lívia", Idade = 17 };
+
+//Chamando o método Envelhecer
+func1.Envelhecer(12); //Somar a idade que você passou para a variável mais 12
+
+Console.WriteLine($"Nome {func1.Nome}, sua idade somada + 12: {func1.Idade}");
+
+//Chamando o método Rejuvelhecer
+func2.Rejuvelhecer(12); //Somar a idade que você passou para a variável menos 12 
+
+Console.WriteLine($"Nome {func2.Nome}, sua idade subtraída - 12 é: {func2.Idade}\n");
+
+
+
+//Instanciando aclasse Funcionário
+Funcionario func3 = new Funcionario { Nome = "Fernanda ", Idade = 19 };
+Funcionario func4 = new Funcionario { Nome = "Fernanda", Idade = 19 };
+
+//Chamando o método Envelhecer
+func3.Envelhecer(10); //Somar a idade que você passou para a variável mais 12
+
+Console.WriteLine($"Nome {func3.Nome}, sua idade somada + 10 é: {func3.Idade}");
+
+//Chamando o método Rejuvelhecer
+func4.Rejuvelhecer(10); //Somar a idade que você passou para a variável menos 12 
+
+Console.WriteLine($"Nome {func4.Nome}, sua idade subtraída - 10 é: {func4.Idade}\n");
+
+
+
+//Instanciando aclasse Funcionário
+Funcionario func5 = new Funcionario { Nome = "Rafaela ", Idade = 23 };
+Funcionario func6 = new Funcionario { Nome = "Rafaela", Idade = 23 };
+
+//Chamando o método Envelhecer
+func5.Envelhecer(5); //Somar a idade que você passou para a variável mais 12
+
+Console.WriteLine($"Nome {func5.Nome}, sua idade somada + 5 é: {func5.Idade}");
+
+//Chamando o método Rejuvelhecer
+func6.Rejuvelhecer(10); //Somar a idade que você passou para a variável menos 12 
+
+Console.WriteLine($"Nome {func6.Nome}, sua idade subtraída - 5 é: {func6.Idade}");
+
+
+
+//Encerrar o programa
+Console.WriteLine("Pressione qualquer  tecla para sair");
+Console.ReadLine();
+
---
+++ Atividade07/Atividade07/Program.cs
@@ -0,0 +1,36 @@
+﻿using Atividade07;
+
+ContaCorrente conta1 = new ContaCorrente
+{
+    Banco = "Bradesco",
+    Agencia = "Brotas",
+    Conta = "Corrente",
+    Saldo = "3000",
+    limite = "1700",
+};
+Console.WriteLine($"Seu banco é: {conta1.Banco}, sua agencia é: {conta1.Agencia}, sua conta é: {conta1.Conta}, " +
+    $"seu saldo é de: {conta1.Saldo}, seu limite é de: {conta1.limite}.");
+
+
+ContaCorrente conta2 = new ContaCorrente
+{
+    Banco = "Caixa",
+    Agencia = "Brotas",
+    Conta = "Corrente",
+    Saldo = "6000",
+    limite = "1900",
+};
+Console.WriteLine($"Seu banco é: {conta2.Banco}, sua agencia é: {conta2.Agencia}, sua conta é: {conta2.Conta}, " +
+    $"seu saldo é de: {conta2.Saldo}, seu limite é de: {conta2.limite}.");
+
+ContaCorrente conta3 = new ContaCorrente
+{
+    Banco = "Santander",
+    Agencia = "Brotas",
+    Conta = "Corrente",
+    Saldo = "8500",
+    limite = "2500",
+};
+Console.WriteLine($"Seu banco é: {conta3.Banco}, sua agencia é: {conta3.Agencia}, sua conta é: {conta3.Conta}, " +
+    $"seu saldo é de: {conta3.Saldo}, seu limite é de: {conta3.limite}.");
+Console.ReadKey();
